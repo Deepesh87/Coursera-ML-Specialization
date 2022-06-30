3c7bdec17d00c5a4102a2938869fee30db60976b
deepesh87 <deepesh123123@gmail.com>
2022-06-30 22:42:45 +0530

    added docstring

--- gradient_descent.py
+++ gradient_descent.py
@@ -7,9 +7,15 @@ import matplotlib.pyplot as plt
 #write the Cost functions
 
 def cost_function(w,b,x,y):
+    """
+computes the cost or Mean Squared Error in case of Linear Regression
+    """
     length=x.shape[0]
     cost=0
     for i in range(length):
         cost+=cost+w*x[i]+b-y[i]
     total_cost=cost/(2*length)
     return total_cost
+
+
+def gradient_descent()
